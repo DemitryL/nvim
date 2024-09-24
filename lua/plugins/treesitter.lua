efbd0e0cff2371aa9925b6638b82601556965e66
DemitryL <dmitryleonovjob@gmail.com>
2024-09-24 16:42:58 +0300

    added go packages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { 
+          ensure_installed = {
               "lua",
 				"javascript",
 				"typescript",
